d287b36b9a308bfd6d0b35e280c7f7bdfef19363
Trevor Li <lili-rovert@hotmail.com>
2018-12-13 20:34:22 -0700

    file structure thingy

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="navbar-dark">
+            <div className=""></div>
+        </div>
+    )
+}
+
+export default Navbar;
